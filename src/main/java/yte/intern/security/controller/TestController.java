9ac1bbd9dad79448458d5d21093b690378586e18
fatih.dogmus <fatih.dogmus@intra.local>
2020-07-20 16:20:50 +0300

    Controller'ın yeri değiştirildi

--- src/main/java/yte/intern/security/controller/TestController.java
+++ src/main/java/yte/intern/security/controller/TestController.java
@@ -1,4 +1,4 @@
-package yte.intern.security;
+package yte.intern.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
