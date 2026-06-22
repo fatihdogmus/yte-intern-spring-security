31fdaddfef6180193f5704b8476d358b9f5340e6
fatihdogmus <fdogmus06@gmail.com>
2026-06-22 11:30:19 +0300

    spring boot güncellendi

--- src/main/java/yte/intern/springsecurity/SpringSecurityApplication.java
+++ src/main/java/yte/intern/springsecurity/SpringSecurityApplication.java
@@ -1,15 +1,12 @@
 package yte.intern.springsecurity;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class SpringSecurityApplication {
 
-
-	public static void main(String[] args) {
+	void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
